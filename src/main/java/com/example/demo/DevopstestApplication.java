e346702bb273b5da7ce02f9b3d159726b407cf27
Neeraja Kuna <neeraja.kuna@nagarro.com>
2024-04-02 15:21:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/DevopstestApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopstestApplication implements CommandLineRunner {
+	
+	public static final Logger logger = LoggerFactory.getLogger(DevopstestApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("its a continuous integration job....");
+		SpringApplication.run(DevopstestApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("its a second logger....");
+		
+	}
+
+}
